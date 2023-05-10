cf794c6b14202ee57f97d6385b3c021bb9f1f982
unknown <happyduo@gmail.com>
2023-05-10 23:02:01 +0100

    portfolio test

---
+++ static/script.js
@@ -0,0 +1,6 @@
+var textArea = document.getElementById("user_input");
+var userInput = textArea.value;
+
+function print_result() {
+    document.getElementById("result").innerHTML = userInput;
+  };
